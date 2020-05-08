12fb67b4d34cc925ab43c8efd723c9d0a5a607ac
YosephKS <yosephkurniasoenggoro@gmail.com>
2020-05-09 03:35:57 +0700

    Adding Matrix Multiplication Implementation in Application sections

--- 2-intro-to-programming-concepts/2.1-basics/basics.py
+++ 2-intro-to-programming-concepts/2.1-basics/basics.py
@@ -1,2 +1,49 @@
-# Printing Hello World
-print("Hello Text")
+# Welcome to Introduction to Python Basics!
+
+# Description:
+
+# In this module, we will look at the overview of the
+# Python programming language and some of basic programming
+# concepts. This module is perfect for those who are starting
+# programming and those who would like to jog their memory
+# after taking some time not programming.
+
+# Author: Yoseph Kurnia Soenggoro
+# Last Updated: Thursday, 20 February 2020
+
+# 1. Printing Text
+print("Hello World")    # By running this instruction, "Hello World" will appear in the terminal
+
+# 2. Using Comments
+# The symbol in front of this line is a comment.
+# Every script after this symbol will be skipped by the computer.
+# However, it is useful to have this as a part of documentation for
+# both the authors and other contributors
+
+# 3. Python as Calculator
+x = 3   # Defining variables (save values)
+X = 2   # Python is case-sensitive, so different case implies different variables
+
+# Using mathematical operation
+y = x + X # y = 5 (Defining a new variable)
+
+x = 4   # With this instruction, x value is overwritten to 4
+        # Nevertheless, it does not affect the value of y
+
+# There are two types of fundamental mathemtaical operations that can be used in Python
+
+# Unary Operators
+p = +2      # positive
+q = -2      # negative
+
+# Binary Operators
+a = 3 + 2   # addition
+b = 3 - 2   # substraction
+c = 3 * 2   # multiplication
+d = 3 / 2   # division
+e = 3 ** 2  # power
+
+# These operations follow the rules of mathematical operation
+# Example
+f = (1 + 1) ** 2 * 3 # f = 2 ** 2 * 3 = 4 * 3 = 12 
+
